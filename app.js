c48ee7ba2a27929f818affc4cf6447b9f5455224
maurobustos5081 <mauro_18_b@hotmail.com>
2023-05-16 19:45:01 -0300

    app de calculadora

---
+++ app.js
@@ -0,0 +1,31 @@
+const process = require("process");
+
+const calculadora = require("./calculadora");
+const { log } = require("console");
+
+const operaciones = ["sumar", "restar", "multiplica", "dividir"]
+
+
+
+const operacion = process.argv[2];
+const numeroA = +process.argv[3];
+const numeroB = +process.argv[4];
+let resultado = 0;
+
+if(operaciones.includes(operacion)){
+   if(operacion === "sumar"){
+      resultado = calculadora.sumar(numeroA,numeroB)
+   } else if(operacion === "restar"){
+       resultado = calculadora.restar(numeroA,numeroB)
+    } else if(operacion === "multiplicar"){
+       resultado = calculadora.multiplicar(numeroA,numeroB)
+    } else if(operacion === "dividir"){
+       resultado = calculadora.dividir(numeroA,numeroB) 
+    }
+    console.log(resultado);
+} else {
+   console.log("ERROR: la operacion no es valida");
+}
+
+
+
